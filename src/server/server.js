7edb04fa66277d62a1e85fe46654e9170473d22f
Sontran-1928 <88760273+Sontran-1928@users.noreply.github.com>
2025-05-22 22:59:29 +1000

    Test vault key

--- src/server/server.js
+++ src/server/server.js
@@ -4,6 +4,18 @@ const app = express()
 const apicache = require('apicache')
 const axios = require('axios')
 const cors = require('cors')
+const az_identity = require('@azure/identity');
+const az_kv = require('@azure/keyvault-secrets');
+
+const credential = new az_identity.DefaultAzureCredential();
+const client = new az_kv.SecretClient('https://newsecretvault.vault.azure.net/', credential)
+
+client.getSecret('APIKEY').then(res =>{
+  console.log(res);
+}).catch(err => {
+  console.log("error occurred, ", err)
+});
+
 require('dotenv').config()
 
 // 36000000 = 10 hours in milliseconds used to refresh fuel oauth token.
